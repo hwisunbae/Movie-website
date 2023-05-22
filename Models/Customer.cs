5e56d72d5eb7420abc2dac574885ab2c6e8c3950
Hwisun Bae <hwisunbae.hb@gmail.com>
2023-05-22 16:17:20 +0100

    finish Section 5 without Exercise

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,19 +7,20 @@ namespace Vidly.Models
 		[Key]
         public int Id { get; set; }
 
-		[Required]
+		[Required(ErrorMessage ="Please enter customer's name.")]
 		[StringLength(255)]
         public string Name { get; set; }
 
 		public bool IsSubscribedNewsletter { get; set; }
 
 		[Display(Name = "Date of Birth")]
-		public DateTime? Birthdate { get; set; }
+        [IsAgeOver18]
+        public DateTime? Birthdate { get; set; }
 
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
-        public MembershipType MembershipType { get; set; }
+        public MembershipType? MembershipType { get; set; }
     }
 }
 
--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -4,13 +4,17 @@ namespace Vidly.Models
 {
 	public class MembershipType
 	{
-		[Key]
 		public byte Id { get; set; }
 
+		[Required]
         public string Name { get; set; }
+
         public short SignUpFee { get; set; }
 		public byte DurationInMonths { get; set; }
 		public byte DiscountRate { get; set; }
-	}
+
+		public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
+    }
 }
 
